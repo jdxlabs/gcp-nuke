85b365cdc29db8f6aae1d674827840959646390a
Jérôme Devoucoux <jdevoucoux@gmail.com>
2025-08-19 18:06:32 +0200

    set role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -5,6 +5,6 @@ resource "google_service_account" "gcp_nuke" {
 
 resource "google_project_iam_member" "gcp_nuke_owner" {
   project = var.project_id
-  role    = "roles/editor"
+  role    = "roles/owner"
   member  = "serviceAccount:${google_service_account.gcp_nuke.email}"
 }
